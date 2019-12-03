89bf5a0c4e8bda749a632283dfbb079b5c088d32
arobinson8 <57469039+arobinson8@users.noreply.github.com>
2019-12-02 17:18:34 -0800

    Final fib

--- fibonacci.v
+++ fibonacci.v
@@ -6,20 +6,33 @@ module fib (
   output reg [3:0] y
 );
   
-  reg [3:0] i;
+  reg [3:0] past;
+  
+  initial
+    begin
+      past = 4'd1;
+      y = 4'd0;
+    end
   
   always @( posedge clk )
     begin 
-      y = i + y;
-      i <= y;
-      
-      if ( y == 0 )
-        
-        y = 1;
-        
-      end
-   
+      if ( y >= 13)
+        begin
+          past <= 4'd1;
+          y <= 4'd0;
+        end
+      else
+        begin
+          y <= y + past;
+          past <= y;
+        end
+    end
+  
 endmodule
+          
+      
+   
+
 
 
 
